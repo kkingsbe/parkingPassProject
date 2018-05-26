d35679a21ae9ec8f6e3a136f3d0479279918c9f1
Kyle <kkingsbe@gmail.com>
2018-05-26 10:55:26 -0400

    Actualy Working

--- Google Sheets Scraper.py
+++ Google Sheets Scraper.py
@@ -42,7 +42,7 @@ def getScore(person):
         if person.get("Do you go directly to an extracurricular activity after school?").lower() == "yes" and int(person.get("If you answered yes to the previous question, how many days do you participate in extracurriculars per week?")) > 0 :  #If they participate in extracurricular activities
             score += 2 + int(person.get("If you answered yes to the previous question, how many days do you participate in extracurriculars per week?"))
         if int(person.get("How many people would you carpool with if you receive the pass? (0 if it's just you)")) > 0:  #How many people would carpool with the person answering
-            score += int(person.get("How many people would you carpool with if you receive the pass? (0 if its just you)")) * 1.5
+            score += int(person.get("How many people would you carpool with if you receive the pass? (0 if it's just you)")) * 1.5
 
         if score > thresholdScore:
             parkingPass = True
@@ -59,6 +59,8 @@ def getScore(person):
 
     except Exception as e:
         print("Error: " + str(e))
+        if "nonetype" in str(e).lower():
+            print("Maybe the text for one of the questions was changed?")
 
 def sendEmail(subject,body,reciever):
     msg = MIMEMultipart()
@@ -98,4 +100,6 @@ while True:
                     sendEmail("Parking Pass Update",body,recieve)
     except Exception as e:
         print("Error: " + str(e))
-    time.sleep(30)
+        if "nonetype" in str(e).lower():
+            print("Maybe the text for one of the questions was changed?")
+    time.sleep(1)
